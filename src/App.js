283d4b68d8a82c2ce87101b873b7c4a4b0a2798e
Daniel Popchenko <104201822+DanielPopchenko@users.noreply.github.com>
2023-08-07 12:32:40 +0300

    made a hero div

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Container } from "./components/container/Container";
 import { Header } from "./components/header/Header";
+import Hero from "./components/hero/Hero";
 
 const App = () => {
 	return (
 		<>
 			<Container>
 				<Header />
+				<Hero />
 			</Container>
 		</>
 	);
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import styles from "./Hero.module.css";
+import image from "../images/hero-img.png";
+import element from "../images/element.png";
 
 const Hero = () => {
-	return <div className={styles.hero}>Hero</div>;
+	return (
+		<div className={styles.hero}>
+			<div className={styles.textContainer}>
+				<div className={styles.rectangle}>LET`S</div>
+				<h2 className={styles.heroHeading}>EXPLORE</h2>
+				<div className={styles.yellowRectangle}>UNIQUE</div>
+				<h2 className={styles.heroHeading}>CLOTHES.</h2>
+				<p>Live for Influential and Innovative fashion!</p>
+				<div className={styles.buttonGroup}>
+					<img src={element} alt="" width={150} heigth={30} />
+					<button type="button">Shop Now</button>
+				</div>
+			</div>
+			<div className={styles.imageContainer}>
+				<img src={image} alt="girl" width={681} height={614} />
+			</div>
+		</div>
+	);
 };
 
 export default Hero;
